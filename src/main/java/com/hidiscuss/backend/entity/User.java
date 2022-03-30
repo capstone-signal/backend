4e7177fa78fff72190aa9dba0dd86fff9344af80
suin <ei654028@gmail.com>
2022-03-30 18:33:35 +0900

    feat: edit User entity

--- src/main/java/com/hidiscuss/backend/entity/User.java
+++ src/main/java/com/hidiscuss/backend/entity/User.java
@@ -6,22 +6,31 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
-@Table(name = "user")
 @Entity
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
+@Table(name = "User")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "idx", nullable = false)
-    private Long idx;
+    private Long id;
 
     @Column(name = "name")
     private String name;
 
     @Column(name = "email")
     private String email;
+
+    @Column(name = "access_token")
+    private String accessToken;
+
+    @Column(name = "refresh_token")
+    private String refresh_token;
+
+    @Column(name = "point")
+    private Long point;
+
+    @OneToMany(mappedBy = "user")
+    private List<Discussion> discussion = new ArrayList<>();
 }
