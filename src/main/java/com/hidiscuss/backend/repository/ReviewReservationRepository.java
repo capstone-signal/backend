bb98bba816408714cd45d0408b247532b02008ae
Donghyeong Kang <dh@uos.ac.kr>
2022-04-20 21:54:36 +0900

    UPDATE ReviewReservation Repository - add method findByDiscussionId

--- src/main/java/com/hidiscuss/backend/repository/ReviewReservationRepository.java
+++ src/main/java/com/hidiscuss/backend/repository/ReviewReservationRepository.java
@@ -3,5 +3,8 @@ package com.hidiscuss.backend.repository;
 import com.hidiscuss.backend.entity.ReviewReservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewReservationRepository extends JpaRepository<ReviewReservation, Long>, ReviewReservationRepositoryCustom {
+
 }
