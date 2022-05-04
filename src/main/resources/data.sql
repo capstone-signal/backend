8ced7b634624ebf1ac3ba3ef4458ebcfa73bb583
suin <ei654028@gmail.com>
2022-05-04 22:19:27 +0900

    data: add discussion_tag to data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -25,5 +25,11 @@ INSERT INTO `hidiscuss`.`live_review_diff` (`id`, `created_at`, `last_modified_a
     (7500, NOW(), NOW(), 'code_after', 7201, 7301);
 
 -- liveReviewReservation(id 7600) 생성
-INSERT INTO `review_reservation` (`id`, `created_at`, `last_modified_at`, `isdone`, `review_start_date`, `reviewee_participated`, `reviewer_participated`, `discussion_id`, `review_id`, `reviewer_id`) VALUES
+INSERT INTO `hidiscuss`.`review_reservation` (`id`, `created_at`, `last_modified_at`, `isdone`, `review_start_date`, `reviewee_participated`, `reviewer_participated`, `discussion_id`, `review_id`, `reviewer_id`) VALUES
     (7600, NOW(), NOW(), 0, '2022-04-27 18:00:02.947000', 0, 0, 7100, NULL, 7000);
+
+-- discussion_tag(id 7700, 7701) 생성
+INSERT INTO `hidiscuss`.`discussion_tag` (`id`, `created_at`, `last_modified_at`, `discussion_id`, `tag_id`) VALUES
+    (7700, NOW(), NOW(), 7100, 1);
+INSERT INTO `hidiscuss`.`discussion_tag` (`id`, `created_at`, `last_modified_at`, `discussion_id`, `tag_id`) VALUES
+    (7701, NOW(), NOW(), 7100, 2);
