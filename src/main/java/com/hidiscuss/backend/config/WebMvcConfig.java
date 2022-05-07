b410e70639acbd11ec427342011d1f96909dbc17
Donghyeong Kang <dh@uos.ac.kr>
2022-05-07 14:26:58 +0900

    FIX: cors config

--- src/main/java/com/hidiscuss/backend/config/WebMvcConfig.java
+++ src/main/java/com/hidiscuss/backend/config/WebMvcConfig.java
@@ -16,8 +16,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("*")
-                .allowedOrigins("http://localhost:3000","https://github.com")
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*").allowCredentials(true);
     }
