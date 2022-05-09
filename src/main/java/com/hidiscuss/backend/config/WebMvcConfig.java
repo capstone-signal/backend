b1113c23f0956f7a5dd004a8d7589c3cf266511c
Donghyeong Kang <dh@uos.ac.kr>
2022-05-10 01:50:56 +0900

    FIX: mvc cors config

--- src/main/java/com/hidiscuss/backend/config/WebMvcConfig.java
+++ src/main/java/com/hidiscuss/backend/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.hidiscuss.backend.config;
 
 import com.hidiscuss.backend.config.interceptor.GitHubClearInterceptor;
 import com.hidiscuss.backend.entity.StringToEnumConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,6 +11,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${spring.profiles.active}")
+    private String profile;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new GitHubClearInterceptor()).order(Integer.MIN_VALUE);
@@ -18,9 +22,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*").allowCredentials(true);
+                .allowedOrigins(profile.equals("dev") ? "http://localhost:3000" : "https://hidiscuss.ga")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
     }
 
     @Override
