8861e83fadb6f937dac111744ab54337a93db964
Hyun_Gwang <mabr2845@gmail.com>
2022-05-26 23:07:26 +0900

    Created At, lastModifiedAt 되게 수정

--- src/main/java/com/hidiscuss/backend/entity/BaseEntity.java
+++ src/main/java/com/hidiscuss/backend/entity/BaseEntity.java
@@ -5,10 +5,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
+import javax.persistence.*;
 import java.time.ZonedDateTime;
 
 @MappedSuperclass
@@ -16,11 +13,20 @@ import java.time.ZonedDateTime;
 @Getter
 public abstract class BaseEntity {
 
-    @CreatedDate
     @Column(name = "created_at", updatable = false, nullable = false)
     private ZonedDateTime createdAt;
 
-    @LastModifiedDate
     @Column(name = "last_modified_at", nullable = false)
     private ZonedDateTime lastModifiedAt;
+
+    @PrePersist
+    private void prePersist() {
+        this.createdAt = ZonedDateTime.now();
+        this.lastModifiedAt = ZonedDateTime.now();
+    }
+
+    @PreUpdate
+    private void preUpdate() {
+        this.lastModifiedAt = ZonedDateTime.now();
+    }
 }
