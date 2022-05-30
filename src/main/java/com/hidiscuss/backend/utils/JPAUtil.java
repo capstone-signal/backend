bb842b87659ac1bcefc45e46df1dd8cf3c43d56b
suin <ei654028@gmail.com>
2022-05-30 15:37:36 +0900

    refactor: generic type parameter

--- src/main/java/com/hidiscuss/backend/utils/JPAUtil.java
+++ src/main/java/com/hidiscuss/backend/utils/JPAUtil.java
@@ -9,7 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
 public class JPAUtil {
-    public static <T, K extends EntityPathBase<T>> JPAQuery<T> paging(Pageable pageable, JPAQuery<T> query, K entityPath) {
+    public static <T1, T2 extends EntityPathBase<T1>> JPAQuery<T1> paging(Pageable pageable, JPAQuery<T1> query, T2 entityPath) {
         query.offset(pageable.getOffset()).limit(pageable.getPageSize());
         for (Sort.Order o : pageable.getSort()) {
             PathBuilder pathBuilder = new PathBuilder(entityPath.getType(), entityPath.getMetadata());
